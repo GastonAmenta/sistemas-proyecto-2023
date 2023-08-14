b63a34b178a62b3993ebf4da590681e446eab30c
Tsifok <hermanmatias03@gmail.com>
2023-08-14 14:13:40 -0300

    mvc

---
+++ controllers/home.php
@@ -0,0 +1,14 @@
+<?php 
+
+/*
+require_once "../../includes/config.php";
+require_once "../../includes/functions.php";
+*/
+
+
+
+$page = "Inicio";
+$section = "home";
+require_once "views/layout.php";
+
+?>
