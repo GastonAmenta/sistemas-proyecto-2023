5a312f98133ada9e2955d8abb92638b7e585bf1b
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-10-09 14:14:55 -0300

    realizando register

--- controllers/register.php
+++ controllers/register.php
@@ -1,12 +1,42 @@
 <?php 
 require_once "../includes/config.php";
 
+if (!empty($_POST)){
+    
+    if($_POST['password'] == $_POST['conf_password']){
 
+        
+            $query_user = "INSERT INTO usuarios(nombre, apellido, nombre_usuario, rol, email, clave, fecha_nac, direccion, fecha_alta) 
+            VALUES('". $_POST['dni']."' , '". $_POST['name']."' , '". $_POST['surname']."' , '". $_POST['email']."' , '". $_POST['birth_date']."' , '". sha1($_POST['password']) ."' , '". $_POST['cell_nmb']."' , '". $_POST['addres']."' , '". $_POST['cuil']."' , '". $_POST['select-register']."' , '". sha1($_POST['answer'])."' , now() );";    
 
+            $result_user = mysqli_query($conn, $query_user);
 
+            if(!$result_user){                
+                die('Error de Consulta ' . mysqli_error($conn));
+            }else{
+
+            //$query_usuario_id = mysqli_insert_id($conn)
+            // mysqli_insert_id($conn);
+                    $sqlLogin = "SELECT * FROM usuarios WHERE DNI='" . $_POST['dni'] . "' ";
+                    $resLogin = mysqli_query($conn, $sqlLogin);
+                    if(!$resLogin){
+                        die("Error de consulta: " . mysqli_error($conn));
+                    }
+                    if (mysqli_num_rows($resLogin) === 1){
+                    $rowLogin = mysqli_fetch_array($resLogin, MYSQLI_ASSOC);
+                
+                    session_start();
+                
+                
+                    $_SESSION["user"] = $rowLogin;
+                   // print_r($_SESSION["user"]);
+                    header('Location: home.php');
+                    }
+                }                        
+            }
+        }
+    
 
 $page = "Inicio";
 $section = "register";
 require_once "../views/layout.php";
-
-?>
--- views/register.php
+++ views/register.php
@@ -1,30 +1,17 @@
 <div class="container">
     <div class="form_container">
-        <form action="../controllers/register.php" method="POST" id="form_register">
-            <div class="input_reg_form"> <input type="number" name="dni" placeholder="DNI" required> </div>
-            <div class="input_reg_form"> <input type="number" name="cuil" placeholder="CUIL/CUIT" required> </div>
+        <form action="../controllers/register.php" method="POST" id="form_register"> 
+
             <div class="input_reg_form"> <input type="text" name="name" placeholder="Nombre" required> </div>
             <div class="input_reg_form"> <input type="text" name="surname" placeholder="Apellido" required> </div>
+            <div class="input_reg_form"> <input type="text" name="username" placeholder="Nombre de usuario" required> </div>
             <div class="input_reg_form"> <input type="email" name="email" placeholder="Email" required> </div>
             <div class="input_reg_form"> <input type="password" name="password" placeholder="Contraseña" required> </div>
             <div class="input_reg_form"> <input type="password" name="conf_password" placeholder="Repetir contraseña" required> </div>
-            <div class="input_reg_form"> <input type="number" name="cell_nmb" placeholder="Telefono" required> </div>
             <div class="input_reg_form"> <input type="text" name="addres" placeholder="Direccion" required> </div>
             <div class="input_reg_form"> <input type="date" name="birth_date" placeholder="Fecha de nacimiento" required> </div>
 
-            <div class="input_reg_form">
-                <select class="register_select" id="select-register" name="select-register" required>
-                    <option hidden selected>Selecciona pregunta de seguridad</option>
-                    <option value=1>En que año entraste a la secundaria ?</option>
-                    <option value=2>Cual es el segundo nombre de tu abuela ?</option>
-                    <option value=3>Cual es el apodo de tu hijo/a ?</option>
-                    <option value=4>Nombre de tu primera mascota ?</option>
-                    <option value=5>Nombre de la primera escuela a la que fuiste ?</option>
-                </select>
-            </div>
-
             <div class="form_container_1">
-                <div class="input_reg_form" style="margin-bottom : 15px"> <input type="text" name="answer" placeholder="Respuesta" required> </div>
                 <div class="btns_register">
                     <button class="btn_reg_form" id="btn_form_register">Registrarme</button>
                     <div class=div_href_login>
