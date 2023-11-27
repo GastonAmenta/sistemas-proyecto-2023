e05225b2be09dad7e7e08cfda6b53d51101daeb8
GastonAmenta <gasty.amenta.et26@gmail.com>
2023-11-27 13:32:40 -0300

    arreglo errores

--- views/administracion.php
+++ views/administracion.php
@@ -1,4 +1,5 @@
 <div class="container">
+<a href="home.php"> <button class="btn_reg_form" id="btn_form_register">Atras</button></a>
     <div class="form_container">
         <div class="btns_register">
             <a href="administracion/verproductos.php"> <button class="btn-admin" id="btn_form_register">Ver productos</button> </a>
--- views/administracion/verproductos.php
+++ views/administracion/verproductos.php
@@ -1,3 +1,3 @@
 <script src="../../js/administracion/verproductos.js"></script>
-
+<a href="../administracion.php"> <button class="btn_reg_form" id="btn_form_register">Atras</button></a>
 <div id="container-allproducts" class="container-allproducts"></div>
